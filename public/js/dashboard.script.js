e2bb6da928ebd25d036089ea1ed71b7719021444
Aissam BELHACHMI <edmeral.aissam@gmail.com>
2014-08-08 11:54:16 +0100

    Improvements in the writing form

--- public/js/dashboard.script.js
+++ public/js/dashboard.script.js
@@ -7,3 +7,19 @@ var progressBar = new Nanobar({
 	id: 'progress-bar'
 });
 
+var submit = $('#submit');
+$('#submit').click(function(e) {
+	e.preventDefault();
+
+	submit.text('Saving..').removeClass('btn-success').addClass('btn-default');
+
+	$.post('./dashboard', $('form').serialize(), function() {
+		submit.text('Saved!');
+		submit.removeClass('btn-default').addClass('btn-success')
+		setTimeout(function() {
+			submit.text('Save');
+		}, 2000);
+	});
+
+});
+
--- routes/dashboard.js
+++ routes/dashboard.js
@@ -2,66 +2,98 @@ var Post = require('../models/post');
 var moment = require('moment');
 
 module.exports = function(app) {
-	app
-		.get('/dashboard', isLoggedIn, function(req, res) {
+  app
+    .get('/dashboard', isLoggedIn, function(req, res) {
 
-				Post.find({ 'username': req.user.username}, function(err, posts) {
-					if (err)
-						res.render('dashboard.ejs', { user: req.user, posts: null });
+        Post.find({ 'username': req.user.username}, function(err, posts) {
+          if (err)
+            res.render('dashboard.ejs', { user: req.user, posts: null });
 
-					else {
-						
-						var noPosts = false; 
-						var noPostToday = false;
+          else {
+            
+            var noPosts = false; 
+            var noPostToday = false;
 
-						if (posts.length === 0) {
-							noPosts = true;
-							noPostToday = true;
-						}
-						else {
-							// if the latest post date field and today's date are not equal
-							// then the user hasn't yet written anything today
-							if (!moment(posts[0].date).isSame(moment(), 'day'))  
-								noPostToday = true; 														
-						} 
+            var length = posts.length;
+            if (length === 0) {
+              noPosts = true;
+              noPostToday = true;
+            }
+            else {
+              // if the latest post date field and today's date are not equal
+              // then the user hasn't yet written anything today
+              if (!moment(posts[length - 1].date).isSame(moment(), 'day'))  
+                noPostToday = true;                             
+            } 
 
-						// An array like the one GitHub uses to draw commits calendar on the user profile
-						// https://github.com/users/Edmeral/contributions_calendar_data?_=1407428689806
-						var postsCalendate = []; 
+            var content = '';
+            if(!noPostToday) 
+              content = posts[length - 1].content;
+            // An array like the one GitHub uses to draw commits calendar on the user profile
+            // https://github.com/users/Edmeral/contributions_calendar_data?_=1407428689806
+            var postsCalendate = []; 
 
-						res.render('dashboard.ejs', { 
-							user: req.user, 
-							posts: posts,
-							noPosts: noPosts,
-							noPostToday: noPostToday
-						});
-					}
-				});
-			})
+            res.render('dashboard.ejs', { 
+              user: req.user, 
+              posts: posts,
+              noPosts: noPosts,
+              noPostToday: noPostToday,
+              content: content
+            });
+          }
+        });
+      })
 
-		.post('/dashboard',isLoggedIn, function(req, res) {
+    .post('/dashboard',isLoggedIn, function(req, res) {
 
-			// var title = req.body.title; I don't need this right now
-			var content = req.body.content;
+      var content = req.body.content;
+      var wordsCount = content.replace(/^\s+|\s+$/g,"").split(/\s+/).length;
 
-			var newPost = new Post();
+      Post.find({ 'username': req.user.username }, function(err, posts) {
 
-			newPost.username = req.user.username;
-			newPost.title = title;
-			newPost.content = content;
-			newPost.wordsCount = content.split(/[\s,;\.:<>"%~#&!*_=@\$\^\?\|\\\{\}\-\+\[\]\(\)\/]+/).length;
+        if (err) console.log(err);
 
-			newPost.save(function(err) {
-				if (err) console.log(err);
-			});
+        else {
+          // Checking if there is already a post that was written today we don't need to create
+          // a new one, we just update it
+          if (posts.length > 0 && moment(posts[posts.length - 1].date).isSame(moment(), 'day')) {
+            var id = posts[posts.length - 1]._id;
+            Post.findById(id, function(err, post) {
+              if (err) console.log(err);
+              else {
+                post.content = content;
+                post.wordsCount = wordsCount;
 
-			res.redirect('/');
-		});
+                post.save(function(err) {
+                  if (err) console.log(err);
+                });
+              }
+            });
+
+          }
+
+          else {
+            var newPost = new Post();
+
+            newPost.username = req.user.username;
+            newPost.content = content;
+            newPost.wordsCount = wordsCount;
+
+            newPost.save(function(err) {
+              if (err) console.log(err);
+            });
+          }
+        }
+      });
+
+      res.redirect('/');
+
+    });
 };
 
 function isLoggedIn(req, res, next) {
-	if (req.isAuthenticated())
-		next();
-	else
-		res.redirect('/');
+  if (req.isAuthenticated())
+    next();
+  else
+    res.redirect('/');
 }
