c9ad3abadbd907f7f5fb2e484546902465dc90b9
Aissam BELHACHMI <edmeral.aissam@gmail.com>
2015-02-19 13:55:27 +0000

    Finishing login modal

--- public/js/home.js
+++ public/js/home.js
@@ -1,3 +1,16 @@
+var loginModal = $('.login-modal');
+
 $('#login a').click(function() {
-  $('.login-modal').addClass('is-visible');
+  loginModal.addClass('is-visible');
+  $('.login-modal input[type=text]').focus();
 });
+
+$('.close-modal').click(function() {
+  loginModal.removeClass('is-visible');
+});
+
+loginModal.click(function(event) {
+  if ($(event.target).is(loginModal))
+    loginModal.removeClass('is-visible');
+}); 
+
