c0f029a01f3a02ca7593b42159e278218fb1ed1b
Aissam BELHACHMI <edmeral.aissam@gmail.com>
2015-02-19 16:36:02 +0000

    Closing modal when hitting ESC

--- public/js/home.js
+++ public/js/home.js
@@ -9,8 +9,15 @@ $('.close-modal').click(function() {
   loginModal.removeClass('is-visible');
 });
 
+// Only closing modal if we click oustside theh login box
 loginModal.click(function(event) {
   if ($(event.target).is(loginModal))
     loginModal.removeClass('is-visible');
-}); 
+});
+
+// Closing modal when clicking ESC button
+$(document).keyup(function(event) {
+  if (event.which == 27)
+    loginModal.removeClass('is-visible');
+})
 
