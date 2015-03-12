41791b1b0548dff6aeb374c549e934c4552dda01
Aissam BELHACHMI <edmeral.aissam@gmail.com>
2015-03-12 21:05:26 +0000

    Creating clickHandler outside the loop

--- public/js/archives.script.js
+++ public/js/archives.script.js
@@ -1,38 +1,43 @@
-$.getJSON('/dashboard/posts', function(posts) {
-  for (var year in posts) {
-    var calDiv = $(document.createElement('div'));
-    var title = $(document.createElement('h4'));
-    title.text(year);
-    calDiv.addClass('cal-heatmap year' + year);
-    calDiv.append(title);
-    calDiv.append(document.createElement('hr'));
-    $('body').append(calDiv);
+function clickHandler(date, nb) {
+  var timestamp = date.getTime() / 1000;
+  if (nb !== null)
+    window.location.href = window.location.protocol + '//' + window.location.host + '/dashboard/view/' + timestamp;
+}
 
-    var cal = new CalHeatMap();
-    var start = new Date(year);
+$(function() {
+  $.getJSON('/dashboard/posts', function(posts) {
+    for (var year in posts) {
+      var calDiv = $(document.createElement('div'));
+      var title = $(document.createElement('h4'));
+      title.text(year);
+      calDiv.addClass('cal-heatmap year' + year);
+      calDiv.append(title);
+      calDiv.append(document.createElement('hr'));
+      $('body').append(calDiv);
 
-     cal.init({
-      start: start,
-      itemName: ['word', 'words'],
-      itemSelector: '.year' + year,
-      data: posts[year],
-      domain: 'month',
-      tooltip: true,
-      cellSize: 13,
-      subDomainTitleFormat: {
-        empty: '0 words on {date}',
-        filled: '{count} {name} {connector} {date}'
-      },
-      legendColors: {
-        min: '#f1c40f',
-        max: '#d35400'
-      },
-      legend: [10, 50, 100, 200],
-      onClick: function(date, nb) {
-        var timestamp = date.getTime() / 1000;
-        if (nb !== null)
-          window.location.href = window.location.protocol + '//' + window.location.host + '/dashboard/view/' + timestamp;
-      }
-    });
-  }
+      var cal = new CalHeatMap();
+      var start = new Date(year);
+
+      cal.init({
+        start: start,
+        itemName: ['word', 'words'],
+        itemSelector: '.year' + year,
+        data: posts[year],
+        domain: 'month',
+        tooltip: true,
+        cellSize: 13,
+        subDomainTitleFormat: {
+          empty: '0 words on {date}',
+          filled: '{count} {name} {connector} {date}'
+        },
+        legendColors: {
+          min: '#f1c40f',
+          max: '#d35400'
+        },
+        legend: [10, 50, 100, 200],
+        onClick: clickHandler
+      });
+    }
+  });
+
 });
