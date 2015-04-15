19dbeec461ded69020ba6c9298d93ecc2fd721a0
Aissam BELHACHMI <edmeral.aissam@gmail.com>
2015-04-15 21:13:14 +0100

    Removing console.log

--- public/js/dashboard.script.js
+++ public/js/dashboard.script.js
@@ -179,31 +179,22 @@ $(function() {
   });
   
   if (typeof Shepherd != 'undefined') {
-    console.log(Shepherd);
-    var tour  = new Shepherd.Tour({
-       defaults: {
-        classes: 'shepherd-theme-arrows'
-       }
-    });
+   var tour;
 
-      tour.addStep('welcome', {
-      text: ['Shepherd is a javascript library for guiding users through your app. It uses <a href="http://github.hubspot.com/tether/">Tether</a>, another open source library, to position all of its steps.', 'Tether makes sure your steps never end up off screen or cropped by an overflow. Try resizing your browser to see what we mean.'],
-      attachTo: '#cal-stats',
-      classes: 'shepherd shepherd-open shepherd-theme-arrows shepherd-transparent-text',
-      buttons: [
-        {
-          text: 'Exit',
-          classes: 'shepherd-button-secondary',
-          action: tour.cancel
-        }, {
-          text: 'Next',
-          action: tour.next,
-          classes: 'shepherd-button-example-primary'
-        }
-      ]
-    });
+   tour = new Shepherd.Tour({
+    defaults: {
+      classes: 'shepherd-theme-arrows'
+    }
+  });
 
-    tour.start();
+   tour.addStep('example', {
+    title: 'Example Shepherd',
+    text: 'Creating a Shepherd is easy too! Just create ...',
+    attachTo: '#content',
+    advanceOn: '.docs-link click'
+  });
+
+   tour.start();
   }
 
   
